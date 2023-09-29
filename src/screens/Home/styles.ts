32587c8de799f9278518ca0ab275562adfa6bd69
Luiz Silva <lasilva@gmail.com>
2023-09-29 19:02:33 -0300

    Movendo os estilos da Home para um arquivo de estilos separado

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,79 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+    },
+    header: {
+        height: 173,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#0D0D0D",
+    },
+    content: {
+        marginTop: -28,
+        paddingHorizontal: 24,
+    },
+    row: {
+        flexDirection: "row",
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 16,
+        height: 54,
+        marginRight: 8,
+        color: "#F2F2F2",
+        fontSize: 16,
+        borderWidth: 1,
+        borderColor: "#0D0D0D",
+        borderRadius: 6,
+        backgroundColor: "#262626",
+    },
+    buttonAdd: {
+        width: 52,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#1E6F9F",
+        borderRadius: 6,
+    },
+    createDone: {
+        paddingTop: 32,
+        paddingBottom: 20,
+        justifyContent: "space-between",
+        borderBottomWidth: 1,
+        borderBottomColor: "#333",
+    },
+    createText: {
+        color: "#4EA8DE",
+        fontSize: 14,
+    },
+    createDoneCounter: {
+        color: "#F2F2F2",
+        fontSize: 12,
+        paddingVertical: 2,
+        paddingHorizontal: 8,
+        marginLeft: 8,
+        borderRadius: 10,
+        backgroundColor: "#333",
+    },
+    doneText: {
+        color: "#8284FA",
+        fontSize: 14,
+    },
+    emptyList: {
+        width: "100%",
+        height: 208,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    emptyListIcon: {
+        marginBottom: 16,
+    },
+    emptyListText: {
+        color: "#808080",
+    },
+    bold: {
+        fontWeight: "bold",
+    },
+});
