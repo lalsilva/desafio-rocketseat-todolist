dc813fceaf4f5cdfd527c83b112993f519a9f625
Luiz Silva <lasilva@gmail.com>
2023-09-30 15:36:40 -0300

    Refatorando os estilos padrões para um arquivo a parte: stylesDefault

---
+++ src/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const stylesDefault = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+    },
+    bold: {
+        fontFamily: "Inter-Bold",
+    },
+    noBorder: {
+        borderTopWidth: 0,
+        borderRightWidth: 0,
+        borderBottomWidth: 0,
+        borderLeftWidth: 0,
+    },
+})
