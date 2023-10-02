5ef19eb9e621652c8880d9d02bdf5c61da9fea50
Luiz Silva <lasilva@gmail.com>
2023-10-02 11:59:29 -0300

    Criado tipo de dados (Type) ref. a Seção

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -16,6 +16,7 @@ import { styles } from "./styles";
 import { Counter } from "../../components/Counter";
 import { Task } from "../../components/Task";
 import { TTask } from "../../components/Task/index.types";
+import { TSection } from "./index.types";
 
 const Logo = require("../../../assets/logo.png");
 const BtnAdd = require("../../../assets/btn-add.png");
@@ -103,7 +104,7 @@ export function Home() {
   }, [tasks]);
 
   useEffect(() => {
-    const sections: Array<any> = [];
+    const sections: Array<TSection> = [];
 
     if (tasksCreated.length > 0) {
       sections.push({
---
+++ src/screens/Home/index.types.ts
@@ -0,0 +1,6 @@
+import { TTask } from "../../components/Task/index.types";
+
+export type TSection = {
+    title: string;
+    data: TTask[];
+}
