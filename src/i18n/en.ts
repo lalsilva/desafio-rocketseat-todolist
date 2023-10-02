5d9f0d3b231e1ad041f1be0f17aac9ec2a282a2c
Luiz Silva <lasilva@gmail.com>
2023-10-02 16:10:26 -0300

    Criados os arquivos para os idiomas pt-BR e en

---
+++ src/i18n/en.ts
@@ -0,0 +1,11 @@
+export const en = {
+    en: {
+        "add a new task": "Add a new task",
+        created: "Created",
+        closed: "Closed",
+        "created tasks": "Created Tasks",
+        "completed tasks": "Completed Tasks",
+        "you don't have tasks registered yet": "You don't have tasks registered yet",
+        "create tasks and organize your to-do items": "Create tasks and organize your to-do items"
+    }
+}
---
+++ src/i18n/pt-BR.ts
@@ -0,0 +1,11 @@
+export const ptBR = {
+    "pt-BR": {
+        "add a new task": "Adicione uma nova tarefa",
+        created: "Criadas",
+        closed: "Concluídas",
+        "created tasks": "Tarefas Criadas",
+        "completed tasks": "Tarefas Concluídas",
+        "you don't have tasks registered yet": "Você ainda não tem tarefas cadastradas",
+        "create tasks and organize your to-do items": "Crie tarefas e organize seus itens a fazer"
+    }
+}
